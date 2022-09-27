5436088a84f03666f7631ff6f94feeb6b0d05bfa
moyoungjun <mo1212123@naver.com>
2022-09-27 18:12:14 +0900

    Fix: model 제거

--- src/main/kotlin/com/example/springboot/controller/MainController.kt
+++ src/main/kotlin/com/example/springboot/controller/MainController.kt
@@ -1,14 +1,12 @@
 package com.example.springboot.controller
 
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class MainController {
     @GetMapping
-    fun test(model: Model): String{
-        model.addAttribute("data","hi")
+    fun test(): String{
         return "main"
     }
 }
