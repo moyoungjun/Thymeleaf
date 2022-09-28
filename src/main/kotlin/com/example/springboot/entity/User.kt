877bbdbe3dba2ed2f30336e2fdc0aab16175ba13
moyoungjun <mo1212123@naver.com>
2022-09-28 17:21:38 +0900

    Fix: UserDetails,BaseTime 상속

--- src/main/kotlin/com/example/springboot/entity/User.kt
+++ src/main/kotlin/com/example/springboot/entity/User.kt
@@ -1,10 +1,13 @@
 package com.example.springboot.entity
 
+import com.example.springboot.config.BaseTime
+import com.example.springboot.dto.UserCreationRequest
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
 import java.time.LocalDate
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
+import javax.validation.constraints.NotNull
 
 @Entity
 class User(
@@ -12,17 +15,71 @@ class User(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
 
+    @Column(nullable = false)
     private val username: String,
 
+    @Column(nullable = false)
     private var password: String,
 
+    @Column(nullable = false)
     var email: String,
 
+    @field:NotNull
+    @Column(nullable = false)
     val birth: LocalDate,
 
+    @Column(nullable = false)
     val code: String,
 
+    @Column(nullable = false)
+    private val role: String
+
+    ): UserDetails, BaseTime() {
+
+    constructor(userCreationRequest: UserCreationRequest): this(
+        username = userCreationRequest.username,
+        password = userCreationRequest.password,
+        email = userCreationRequest.email,
+        birth = userCreationRequest.birth,
+        code = userCreationRequest.code,
+        role = "ROLE_USER"
+    )
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        val authorities = ArrayList<GrantedAuthority>()
+        authorities.add(SimpleGrantedAuthority(role))
+        return authorities.toMutableSet()
+    }
+
+    override fun getPassword(): String {
+        return password
+    }
+
+    fun setPassword(password: String) {
+        this.password = password
+    }
 
 
-    ) {
+    override fun getUsername(): String {
+        return username
+    }
+
+
+    override fun isAccountNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        return true
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isEnabled(): Boolean {
+        return true
+    }
+
+
 }
