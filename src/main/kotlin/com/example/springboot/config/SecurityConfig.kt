ebf7e577038ae00c9f86ed751e05ab21d50a13f3
moyoungjun <mo1212123@naver.com>
2022-09-28 17:20:45 +0900

    Fix: Security 환경설정 변경

--- src/main/kotlin/com/example/springboot/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/springboot/config/SecurityConfig.kt
@@ -1,9 +1,40 @@
 package com.example.springboot.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @EnableWebSecurity
 @Configuration
 class SecurityConfig {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeRequests {
+                authorizeRequests ->
+                  authorizeRequests.antMatchers("/static/**","/register").permitAll()
+                      .anyRequest().authenticated()
+            }
+            .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .failureUrl("/login?error")
+            .and()
+            .logout()
+            .logoutSuccessUrl("/")
+        return http.build()
+    }
+
+
 }
